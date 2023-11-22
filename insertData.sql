7eaec347674e1bb13bd5a473b3aaf10ac2f931c7
Ho Nyeong <dev.honing@gmail.com>
2023-11-22 21:10:25 +0900

    insertData.sql을 생성하고 INSERT INTO를 이용해 새로운 멤버의 정보를 추가하는 SQL 쿼리 작성

---
+++ insertData.sql
@@ -0,0 +1,5 @@
+-- insertData.sql
+
+-- team_members 테이블에 새로운 멤버 정보를 추가하는 SQL 쿼리
+INSERT INTO team_members (name, bio, contact, current_project, img_url)
+VALUES ('새로운멤버', '새로운 소개', '010-1234-5678', '프로젝트A', '이미지URL');
