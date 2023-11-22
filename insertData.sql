bf43a251421052461d85b2cd02fee2e24a22ccef
Ho Nyeong <dev.honing@gmail.com>
2023-11-22 21:32:32 +0900

    insertData.sql의 INSERT INTO에 대한 주석 추가

--- insertData.sql
+++ insertData.sql
@@ -1,5 +1,5 @@
 -- insertData.sql
 
 -- team_members 테이블에 새로운 멤버 정보를 추가하는 SQL 쿼리
-INSERT INTO team_members (name, bio, contact, current_project, img_url)
-VALUES ('새로운멤버', '새로운 소개', '010-1234-5678', '프로젝트A', '이미지URL');
+INSERT INTO team_members (name, bio, contact, current_project, img_url) -- team_members의 항목에 대응하는 value를 데이터로 추가
+VALUES ('새로운 멤버', '새로운 소개', '010-1234-5678', '프로젝트 A', '이미지 URL');
